5605234e0201960354f0f840728406a0e9b45153
tommyelechandro <jasroops1995@gmail.com>
2026-07-26 21:29:17 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,57 +1,85 @@
-const { Client, GatewayIntentBits } = require("discord.js");
+const { 
+    Client, 
+    GatewayIntentBits, 
+    REST, 
+    Routes 
+} = require("discord.js");
+
 require("dotenv").config();
-const config = require("./config.js");
+
+const commands = require("./commands.js");
 const handleEvents = require("./events.js");
 const http = require("http");
-const ranks = [
-    "Praktikant",
-    "Junior Fahrer",
-    "Fahrer",
-    "Erfahrener Fahrer",
-    "Senior Fahrer",
-    "Diamond Fahrer Azubi",
-    "Diamond Fahrer",
-    "Erweiterter Schutzfahrer",
-    "Rechtsanwalt",
-    "Leitstelle",
-    "Security",
-    "Security Chef",
-    "Personalleitung",
-    "Geschäftsleitung Airport",
-    "Geschäftsleitung",
-    "CEO"
-];
+
 
 const client = new Client({
+
     intents: [
         GatewayIntentBits.Guilds,
         GatewayIntentBits.GuildMembers
     ]
-});
-const { REST, Routes } = require("discord.js");
-const commands = require("./commands.js");
 
-const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
+});
+
+
+// Slash Commands registrieren
+const rest = new REST({ version: "10" })
+    .setToken(process.env.TOKEN);
+
+
+
 client.once("ready", async () => {
+
     console.log(`Bot online als ${client.user.tag}`);
 
+
     try {
-await rest.put(
-    Routes.applicationGuildCommands(
-        client.user.id,
-        "1457890146753183918"
-    ),
-    { body: commands }
-);
+
+        await rest.put(
+
+            Routes.applicationGuildCommands(
+                client.user.id,
+                "1457890146753183918"
+            ),
+
+            {
+                body: commands
+            }
+
+        );
+
 
         console.log("Slash-Befehle wurden registriert!");
+
+
     } catch (error) {
+
         console.error(error);
+
     }
+
 });
+
+
+
+// Render Verbindung offen halten
 http.createServer((req, res) => {
+
     res.write("Diamond Taxi Bot läuft!");
+
     res.end();
+
 }).listen(3000);
-client.on("interactionCreate", handleEvents);
+
+
+
+// Events verbinden
+client.on(
+    "interactionCreate",
+    handleEvents
+);
+
+
+
+// Bot starten
 client.login(process.env.TOKEN);
