b348f74686d75b7b8d11160e2d78c4bc3c4678fc
tommyelechandro <jasroops1995@gmail.com>
2026-07-26 20:59:16 +0200

    Update commands.js

--- commands.js
+++ commands.js
@@ -1,38 +1,47 @@
 const { SlashCommandBuilder } = require("discord.js");
 
+const rangAuswahl = [
+    { name: "Praktikant", value: "Praktikant" },
+    { name: "Junior Fahrer", value: "Junior Fahrer" },
+    { name: "Fahrer", value: "Fahrer" },
+    { name: "Erfahrener Fahrer", value: "Erfahrener Fahrer" },
+    { name: "Senior Fahrer", value: "Senior Fahrer" },
+    { name: "Diamond Fahrer Azubi", value: "Diamond Fahrer Azubi" },
+    { name: "Diamond Fahrer", value: "Diamond Fahrer" },
+    { name: "Erweiterter Schutzfahrer", value: "Erweiterter Schutzfahrer" },
+    { name: "Rechtsanwalt", value: "Rechtsanwalt" },
+    { name: "Leitstelle", value: "Leitstelle" },
+    { name: "Security", value: "Security" },
+    { name: "Security Chef", value: "Security Chef" },
+    { name: "Personalleitung", value: "Personalleitung" },
+    { name: "Geschäftsleitung Airport", value: "Geschäftsleitung Airport" },
+    { name: "Geschäftsleitung", value: "Geschäftsleitung" },
+    { name: "CEO", value: "CEO" }
+];
+
 module.exports = [
+
     new SlashCommandBuilder()
         .setName("beförderung")
-    .addStringOption(option =>
-    option
-        .setName("rang")
-        .setDescription("Neuer Rang")
-        .setRequired(true)
-        .addChoices(
-            { name: "Praktikant", value: "Praktikant" },
-            { name: "Junior Fahrer", value: "Junior Fahrer" },
-            { name: "Fahrer", value: "Fahrer" },
-            { name: "Erfahrener Fahrer", value: "Erfahrener Fahrer" },
-            { name: "Senior Fahrer", value: "Senior Fahrer" },
-            { name: "Diamond Fahrer Azubi", value: "Diamond Fahrer Azubi" },
-            { name: "Diamond Fahrer", value: "Diamond Fahrer" },
-            { name: "Erweiterter Schutzfahrer", value: "Erweiterter Schutzfahrer" },
-            { name: "Rechtsanwalt", value: "Rechtsanwalt" },
-            { name: "Leitstelle", value: "Leitstelle" },
-            { name: "Security", value: "Security" },
-            { name: "Security Chef", value: "Security Chef" },
-            { name: "Personalleitung", value: "Personalleitung" },
-            { name: "Geschäftsleitung Airport", value: "Geschäftsleitung Airport" },
-            { name: "Geschäftsleitung", value: "Geschäftsleitung" },
-            { name: "CEO", value: "CEO" }
-        )
-)
         .setDescription("Befördert einen Mitarbeiter")
         .addUserOption(option =>
             option
                 .setName("mitarbeiter")
                 .setDescription("Mitarbeiter auswählen")
                 .setRequired(true)
+        )
+        .addStringOption(option =>
+            option
+                .setName("rang")
+                .setDescription("Neuer Rang")
+                .setRequired(true)
+                .addChoices(...rangAuswahl)
+        )
+        .addStringOption(option =>
+            option
+                .setName("nachricht")
+                .setDescription("Optionale Nachricht")
+                .setRequired(false)
         ),
 
     new SlashCommandBuilder()
@@ -43,6 +52,19 @@ module.exports = [
                 .setName("mitarbeiter")
                 .setDescription("Mitarbeiter auswählen")
                 .setRequired(true)
+        )
+        .addStringOption(option =>
+            option
+                .setName("rang")
+                .setDescription("Neuer Rang")
+                .setRequired(true)
+                .addChoices(...rangAuswahl)
+        )
+        .addStringOption(option =>
+            option
+                .setName("grund")
+                .setDescription("Grund der Degradierung")
+                .setRequired(true)
         ),
 
     new SlashCommandBuilder()
@@ -52,6 +74,12 @@ module.exports = [
             option
                 .setName("mitarbeiter")
                 .setDescription("Mitarbeiter auswählen")
+                .setRequired(true)
+        )
+        .addStringOption(option =>
+            option
+                .setName("grund")
+                .setDescription("Grund der Kündigung")
                 .setRequired(true)
         )
 ];
