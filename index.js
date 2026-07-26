e0d11c02b413598a7892503acd2eafccef70c83a
tommyelechandro <jasroops1995@gmail.com>
2026-07-26 18:42:22 +0200

    Update index.js

--- index.js
+++ index.js
@@ -13,3 +13,21 @@ client.once("ready", () => {
 });
 
 client.login(process.env.TOKEN);
+const ranks = [
+    "Praktikant",
+    "Junior Fahrer",
+    "Fahrer",
+    "Erfahrener Fahrer",
+    "Senior Fahrer",
+    "Diamond Fahrer Azubi",
+    "Diamond Fahrer",
+    "Erweiterter Schutzfahrer",
+    "Rechtsanwalt",
+    "Leitstelle",
+    "Security",
+    "Security Chef",
+    "Personalleitung",
+    "Geschäftsleitung Airport",
+    "Geschäftsleitung",
+    "CEO"
+];
