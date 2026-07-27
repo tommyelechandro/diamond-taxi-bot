1a4593d73d1218fec5c52e1caa90e7700a07eb3a
tommyelechandro <jasroops1995@gmail.com>
2026-07-27 22:26:16 +0200

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,9 @@ module.exports = {
     leitungRolle: "1489698355507499169",
 
     // Kanal für automatische Team-Updates
+    // WICHTIG: Hier die echte Kanal-ID eintragen (Rechtsklick auf den Kanal -> ID kopieren,
+    // Entwicklermodus muss in Discord unter Einstellungen -> Erweitert aktiviert sein).
+    // Solange hier der Platzhalter steht, wird die Teamliste einfach übersprungen.
     teamUpdateKanal: "HIER_KANAL_ID_EINTRAGEN",
 
 
